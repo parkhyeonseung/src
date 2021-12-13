2cfdceec25790461585a4cb45156ed2fbc4c90e0
parkheonseung <gus0tmd@naver.com>
2021-12-13 11:39:26 +0900

    5

--- topic_first/example_B_pub2.py
+++ topic_first/example_B_pub2.py
@@ -3,12 +3,14 @@ import rospy
 from std_msgs.msg import String
 
 if __name__=='__main__':
+    i=0
     rospy.init_node('pub_B2')
     pub = rospy.Publisher('pub_B2',String,queue_size=10)
     rate = rospy.Rate(10)
 
     while True :
-        str = 'im pub_b2 '
-        pub.publish(str)
+        strb = 'im pub_b2 '
+        pub.publish(strb+str(i))
         rate.sleep()
+        i+=1
     pass
